d311ce2c6ffab0d5461a9106b2656d4a47cb4248
3andan4 <maxime.le-besnerais@epitech.eu>
2022-11-01 12:50:54 +0100

    :sparkles: Clean and simple README.md:     docs: Documentation: Actualized install method :memo: giti:     chore: Other: nothing much :adhesive_bandage: install.sh:     style: :wastebasket: Code cleanup: simpler code: Deleted some git clone parts. The script only copy the shortcut in user/bin now. Everything else isn't useful anyway. :art:

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Giti Powercharged
 Python script to write commit message following the [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/) specification.
 This is a tool and not perfect, so please use it with caution.
+
+## Installation
+```bash
+cd ~ && git clone https://github.com/3andan4/giti_super.git ./.giti && ./.giti/install.sh
+```
--- install.sh
+++ install.sh
@@ -8,22 +8,4 @@ if ! [ -x "$(command -v python)" ]; then
     exit 1
 fi
 
-# move giti and pe to /usr/local/bin
-sudo cp ./giti /usr/bin
-
-# check current path
-if [ -d ~/.giti ]; then
-    echo "giti is already installed"
-    echo "erasing old installation"
-    rm -rf ~/.giti
-    cd /home/$user/
-    git clone -q https://github.com/3andan4/giti_super.git ./.giti
-    echo "giti updated"
-else
-    cd /home/$user/
-    git clone -q https://github.com/3andan4/giti_super.git ./.giti
-    echo "giti installed"
-fi
-
-rm -rf ~/.giti/giti
-rm -rf ~/.giti/install.sh
+sudo cp ~/.giti/giti /usr/bin/giti
