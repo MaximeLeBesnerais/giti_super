07f0ffe265a4ac3cf0588bf13c6e5903caa39f6c
3andan4 <maxime.le-besnerais@epitech.eu>
2022-11-01 17:00:05 +0100

    :sparkles: Git on the clock - Command support commands/commands.json:     feat(functionalities): New feature: New commands: Added several new commands to the json file, there are now 15. Will trim some of them for better readability most probably :zap: giti_interface.py:     feat(frame & text): New feature: json management: Get every commands from the json file, and their description for the help command :zap:

--- giti_interface.py
+++ giti_interface.py
@@ -1,33 +1,54 @@
 import os
 import sys
+
+from click import command
 from giti_powercharged import powercharged
+import json
+
+# create a dictionary of commands with the commands/commands.json file
+commands_list = []
+description = {}
+
+def load_commands():
+    global commands_list
+    file_data = json.load(open('commands/commands.json'))
+    for entry in file_data:
+        command_name = entry['command']
+        if isinstance(command_name, list):
+            for command in command_name:
+                commands_list.append(command)
+                description[command] = entry['description']
+        else:
+            commands_list.append(command_name)
+            description[command_name] = entry['description']
+
 
 def main(params):
-    # if params[1] is null or equals to "powercharged":
+    global commands_list, description
     if len(params) == 1 or params[1] == "powercharged":
         powercharged()
-    elif params[1] == 'interface':
-        pass
+    elif params[1] in commands_list:
+        print("received command: " + params[1])
     elif params[1] == 'help':
         print('''
         giti is a git commit interface that allows you to make commits without having to type them out.
         To use giti, type 'giti' followed by the command you want to run.
         The commands are:
         - powercharged (or none): a commit interface that allows you to make commits with extensive help.
-        - feat [file_name]: a commit interface that allows you to make commits for new features
-        - bug [file_name]: a commit interface that allows you to make commits for bug fixes
-        - refactor [file_name]: a commit interface that allows you to make commits for refactoring
-        - style [file_name]: a commit interface that allows you to make commits for code cleanup
-        - chore [file_name]: a commit interface that allows you to make commits for other changes
-        - docs [file_name]: a commit interface that allows you to make commits for documentation
-        - config [file_name]: a commit interface that allows you to make commits for configuration changes
-        - delete [file_name]: a commit interface that allows you to make commits for file deletions
         - help: displays this message.
         ''')
+        x = 0
+        for command in commands_list:
+            print(f'''- {command}: {description[command]}''')
+            x += 1
+            if x == 6:
+                x = 0
+                input("----------")
     else:
         print("Command not found. Type 'giti help' for help.")
 
 
 if __name__ == '__main__':
+    load_commands()
     main(sys.argv)
 
