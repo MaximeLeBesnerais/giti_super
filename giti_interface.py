345fa83cbb705595785150121c24c5386f38fb39
3andan4 <maxime.le-besnerais@epitech.eu>
2022-11-01 17:26:27 +0100

    :sparkles: Work in progress - File by file giti giti_interface.py:     chore: :construction: trying to evaluate if a file is useful for github :question:

--- giti_interface.py
+++ giti_interface.py
@@ -1,16 +1,28 @@
 import os
 import sys
-
-from click import command
 from giti_powercharged import powercharged
 import json
 
 # create a dictionary of commands with the commands/commands.json file
 commands_list = []
 description = {}
+command_input = {}
+message = {}
+
+os.popen('clear')
+os.popen('git add .').readlines()
+git_status = os.popen('git status -s').readlines()
+
+def git_follow(file_name):
+    global git_status
+    for line in git_status:
+        if line[3:] == file_name:
+            return True
+    return False
+
 
 def load_commands():
-    global commands_list
+    global commands_list, description, command_input, message
     file_data = json.load(open('commands/commands.json'))
     for entry in file_data:
         command_name = entry['command']
@@ -18,17 +30,35 @@ def load_commands():
             for command in command_name:
                 commands_list.append(command)
                 description[command] = entry['description']
+                command_input[command] = entry['input']
+                message[command] = entry['message']
         else:
             commands_list.append(command_name)
             description[command_name] = entry['description']
+            command_input[command_name] = entry['input']
+            message[command_name] = entry['message']
+
+
+def command_interpreter(command, file):
+    print("received command: " + command)
+    print("Evaluated file: " + file)
+    if git_follow(file):
+        print("File is interesting for git")
+    else:
+        print("Git doesn't care about this file")
+
 
 
 def main(params):
-    global commands_list, description
+    global commands_list, description, command_input, message
+
     if len(params) == 1 or params[1] == "powercharged":
         powercharged()
     elif params[1] in commands_list:
-        print("received command: " + params[1])
+        if len(params) < 3:
+            print("You need to specify a file")
+            return
+        command_interpreter(params[1], params[2])
     elif params[1] == 'help':
         print('''
         giti is a git commit interface that allows you to make commits without having to type them out.
