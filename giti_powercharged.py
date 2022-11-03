cd6605376681f6f2379c6146a3adea66e5661803
3andan4 <maxime.le-besnerais@epitech.eu>
2022-11-03 10:04:50 +0100

    :sparkles: Giti Rework giti_powercharged.py: 		    refactor: Smaller giti_powercharge -- As most commands are interface based, giti powercharged now gives a smaller choice. It is just easier to understand. :recycle:

--- giti_powercharged.py
+++ giti_powercharged.py
@@ -15,17 +15,15 @@ def powercharged():
     def no_l_b(text_with_line_break):
         return text_with_line_break.replace("\n", "")
 
-    menu = {1: "New Feature", 2: "Bug Fix",
-            3: "Refactoring", 4: "Code Cleanup", 5: "Other", 6: "Delete", 7: "Documentation", 8: "Configuration"}
+    menu = {1: "New Feature", 2: "Bug Fix", 3: "Documentations"}
     if os.path.exists(".gitignore"):
         with open(".gitignore", "r") as f:
             gitignore = f.readlines()
 
     git_status = os.popen('git status -s').readlines()
     for line in git_status:
-        if line[3:] in gitignore:
-            continue
-        else:
+        # Check if file was deleted
+        if not line.startswith(' D'):
             print("for the file: " + line[3:] + " what did you do?")
             for key, value in menu.items():
                 print(key, value)
@@ -45,35 +43,18 @@ def powercharged():
                 bug_fix = input('What did you do to fix it? ')
                 commit_message += "    fix: " + bug_origin + ": " + bug_fix + ":bug:\n"
             elif choice == '3':
-                refactoring_scope = input('What is the scope of the refactoring? ')
-                commit_message += "    refactor: " + refactoring_scope + " :recycle:\n"
-            elif choice == '4':
-                code_cleanup_scope = input(
-                    'What is the scope of the code cleanup? ')
-                code_cleanup_details = input('What did you do to clean the code? ')
-                commit_message += "    style: :wastebasket: Code cleanup: " + \
-                    code_cleanup_scope + ": " + code_cleanup_details + " :art:\n"
-            elif choice == '5':
-                other_summary = input(
-                    'What was the summary of your other changes? ')
-                commit_message += "    chore: Other: " + \
-                    other_summary + " :adhesive_bandage:\n"
-            elif choice == '6':
-                deletion_reason = input('Why did you delete this file? ')
-                commit_message += "    chore: Deletion: " + deletion_reason + " :fire:\n"
-            elif choice == '7':
                 documentation_summary = input('What did you document? ')
                 commit_message += "    docs: Documentation: " + \
                     documentation_summary + " :memo:\n"
-            elif choice == '8':
-                config_name = input('What configuration did you change / add ? ')
-                config_utility = input(
-                    'What is the utility of this configuration? ')
-                commit_message += "    chore: Configuration: " + config_name + ": " + \
-                    config_utility + " :wrench:\n"
             else:
                 weird_comment = choice
                 commit_message += "    chore: " + weird_comment + " :question:\n"
+        else:
+            print("The file " + line[3:] + " was deleted")
+            commit_message += no_l_b(line[3:]) + ": " + "\n"
+            deleted_file_comment = input("Why did you delete it? ")
+            commit_message += "     deletion: " + deleted_file_comment + " :fire:\n"
+
 
     commit_title = ":sparkles: " + input('What is the title of the commit? ')
     final_commit_message = commit_title + "\n" + commit_message
