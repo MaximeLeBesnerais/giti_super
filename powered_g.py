30b1ffb1cc8f22c65a0bdf24f40458389c09f7ee
Maxime <maxime.le-besnerais@epitech.eu>
2023-02-22 10:15:51 +0100

    [WIP] - Functions     powered_g.py: Deleted previous functions (automatic mode) and using this file for 1 purposes functions

--- powered_g.py
+++ powered_g.py
@@ -1,63 +1,59 @@
-#!/usr/bin/python3
 import os
 
+status_letters = {
+    'M': 'This file has been modified',
+    'A': 'This is a new file',
+    'D': 'This is a file suppression',
+    'R': 'This file has been renamed or moved',
+    'C': 'This file has been copied',
+    'U': 'Due to a merge conflict, this file is unmerged',
+    '??': 'As far as git is concerned, this file is untracked'
+}
 
-def no_l_b(text_with_line_break):
-    return text_with_line_break.replace("\n", "")
 
-
-def powercharged():
-
-    commit_message = ""
-
-    menu = {1: "New Feature", 2: "Bug Fix", 3: "Documentations", 4: "Refactoring"}
-
+def giti_all(potential_title):
+    os.system('git add .')
     git_status = os.popen('git status -s').readlines()
+    commit_line = []
     if len(git_status) == 0:
         print("There are no changes to commit")
         exit()
     for line in git_status:
-        if line.startswith(' D') or line.startswith('D'):
-            print("The file " + line[3:] + " was deleted")
-            commit_message += no_l_b(line[3:]) + ": " + "\n"
-            deleted_file_comment = input("Why did you delete it? ")
-            commit_message += "     deletion: " + deleted_file_comment + " :fire:\n"
-        else:
-            print("for the file: " + line[3:] + " what did you do?")
-            for key, value in menu.items():
-                print(key, value)
-            commit_message += no_l_b(line[3:]) + ": " + "\n"
-            choice = str(input('What did you do? '))
-
-            if choice == '1':
-                feature_explication = input('What is the feature about? ')
-                optional_feature_scope = input(
-                    'What is the scope of the feature? (ie: lang) ')
-                if optional_feature_scope != "":
-                    optional_feature_scope = "(" + optional_feature_scope + ")"
-                commit_message += "    feat" + optional_feature_scope + ": New feature: " + \
-                    feature_explication + " :zap:\n"
-
-            elif choice == '2':
-                bug_fix = input('What bug was fixed ? ')
-                commit_message += "    fix: " + bug_fix + " :bug:\n"
-
-            elif choice == '3':
-                commit_message += "    docs: - Documentation was altered - refer to commit title :memo:\n"
-
-            elif choice == '4':
-                commit_message += "    refactor: - Code was refactored - refer to commit title :recycle:\n"
-            else:
-                weird_comment = choice
-                commit_message += "    chore: " + weird_comment + " :question:\n"
-    commit_title = ":sparkles: " + input('What is the title of the commit? ')
-    final_commit_message = commit_title + "\n" + commit_message
-    os.wait()
-    os.popen('git add .')
-    os.wait()
-    os.system('git commit -m "' + final_commit_message + '"')
-    os.system('git pull')
+        status_letter = line[:2]
+        status_comment = status_letters[status_letter]
+        commit_line.append(f"\t{line[3:]}: {status_comment}")
+    if potential_title == "":
+        commit_message = f"[ALL] {len(git_status)} files changed:\n" + "\n".join(commit_line)
+    else:
+        commit_message = f"[ALL] {potential_title}:\n" + "\n".join(commit_line)
+    os.system(f'git commit -m "{commit_message}"')
 
 
-if __name__ == "__main__":
-    powercharged()
+def giti_makefile(potential_title):
+    git_log = os.popen('git status -s').readlines()
+    commit_line = []
+    if len(git_log) == 0:
+        print("There are no changes to commit")
+        exit()
+    # for each line that contains a Makefile, add it to the commit
+    for line in git_log:
+        if line[3:].startswith('Makefile'):
+            os.system(f'git add {line[3:]}')
+            commit_line.append(f"\t{line[3:]}: {status_letters[line[:2]]}")
+    if potential_title == "":
+        commit_message = f"[MAKEFILE] {len(commit_line)} files changed:\n" + "\n".join(commit_line)
+    else:
+        commit_message = f"[MAKEFILE] {potential_title}:\n" + "\n".join(commit_line)
+    os.system(f'git commit -m "{commit_message}"')
+
+
+def giti_coding_style(potential_title, files):
+    commit_line = []
+    for file in files:
+        os.system(f'git add {file}')
+        commit_line.append(f"\t{file}: Coding style related changes")
+    if potential_title == "":
+        commit_message = f"[CODING STYLE] {len(commit_line)} files changed:\n" + "\n".join(commit_line)
+    else:
+        commit_message = f"[CODING STYLE] {potential_title}:\n" + "\n".join(commit_line)
+    os.system(f'git commit -m "{commit_message}"')
