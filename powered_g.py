930c4bf7c1b4650fe46310dded6364dbd568d4ec
Maxime <maxime.le-besnerais@epitech.eu>
2023-02-27 13:14:45 +0100

    [BUG FIX] 2 files changed 	giti: This file has been modified 	powered_g.py: This file has been modified 	Committed without a comment

--- powered_g.py
+++ powered_g.py
@@ -18,12 +18,14 @@ command_types = {
     "wip": "[WIP]",
     "header": "[HEADER]",
     "assets": "[ASSETS]",
+    "make": "[MAKEFILE]"
 }
 
 
 def giti_all(potential_title):
     os.system('git add .')
     git_status = os.popen('git status -s').readlines()
+    git_status = [line[:-1] for line in git_status]
     commit_line = []
     if len(git_status) == 0:
         print("There are no changes to commit")
@@ -42,6 +44,7 @@ def giti_all(potential_title):
 
 def giti_makefile(potential_title):
     git_log = os.popen('git status -s').readlines()
+    git_log = [line[:-1] for line in git_log]
     commit_line = []
     if len(git_log) == 0:
         print("There are no changes to commit")
@@ -88,10 +91,21 @@ def giti_header(potential_title):
 
 
 def giti_coding_style(files):
+    git_log = os.popen('git status -s').readlines()
+    git_log = [line[:-1] for line in git_log]
     commit_line = []
-    for file in files:
-        os.system(f'git add {file}')
-        commit_line.append(f"\t{file}: Coding style related changes")
+    if len(git_log) == 0:
+        print("There are no changes to commit")
+        exit()
+    for line in git_log:
+        if line[3:] in files:
+            os.system(f'git add {line[3:]}')
+            letter = line[:2].strip()
+            commit_line.append(f"\t{line[3:]}: {status_letters[letter]}")
+            continue
+    if len(commit_line) == 0:
+        print("There are no files to commit")
+        exit()
     commit_message = f"[CODING STYLE] {len(commit_line)} {'file' if len(commit_line) <= 1 else 'files'} changed\n" + \
                      "\n".join(commit_line)
     os.system(f'git commit -m "{commit_message}"')
@@ -138,6 +152,7 @@ def generic_giti(tag_key, title, comment, files: list):
         exit()
     commit_line = []
     git_log = os.popen('git status -s').readlines()
+    git_log = [line[:-1] for line in git_log]
     tag = command_types[tag_key] if tag_key in command_types else f"[{tag_key.upper()}]"
     for file in files:
         os.system(f'git add {file}')
@@ -152,64 +167,4 @@ def generic_giti(tag_key, title, comment, files: list):
     else:
         commit_line.append(f"\tCommitted without a comment")
     commit_message = potential_title + "\n".join(commit_line)
-    os.system(f'git commit -m "{commit_message}"')
-
-
-def giti_interactive():
-    git_log = os.popen('git status -s').readlines()
-    committing_files = []
-    if len(git_log) == 0:
-        print("There are no changes to commit")
-        exit()
-    adding_files = True
-    if len(git_log) == 1:
-        print("There is only one file to commit, adding it automatically")
-        committing_files.append(git_log[0][3:])
-        adding_files = False
-    while adding_files:
-        if len(git_log) == 1:
-            print("There is only one file to commit, do you want to add it?(y/n)")
-            if input() == "y":
-                committing_files.append(git_log[0][3:])
-                print("File added")
-            adding_files = False
-            break
-        for i, line in enumerate(git_log):
-            print(f"{i + 1}. {line[3:]}")
-        input_number = input("Which file do you want to add to the commit? (0 to stop adding files) ")
-        if input_number == "0" or input_number == "":
-            adding_files = False
-        else:
-            try:
-                input_number = int(input_number)
-                if input_number <= 0 or input_number > len(git_log):
-                    print("Invalid input")
-                else:
-                    committing_files.append(git_log[input_number - 1][3:])
-                    git_log.pop(input_number - 1)
-                    adding_files = len(git_log) != 0
-            except ValueError:
-                print("Invalid input")
-    if len(committing_files) == 0:
-        print("No files to commit")
-        exit()
-    commit_line = []
-    for file in committing_files:
-        os.system(f'git add {file}')
-        letter = git_log[0][:2].strip()
-        file = file[:-1]
-        commit_line.append(f"\t{file}: {status_letters[letter]}")
-    commit_type = input("What type of commit is this ? (default: [OTHER]) ")
-    if commit_type == "":
-        commit_type = "[OTHER]"
-    else:
-        commit_type = f"[{commit_type.upper()}]"
-    commit_title = input("What is the title of the commit ? (default: giti default commit) ")
-    if commit_title == "":
-        commit_title = f"{commit_type} {len(committing_files)} {'file' if len(committing_files) <= 1 else 'files'}" \
-                       f" changed"
-    commit_comment = input("Do you want to add a comment to the commit ? (default: no comment) ")
-    if commit_comment == "":
-        commit_comment = "Committed without a comment"
-    commit_message = f"{commit_type} {commit_title}\n" + "\n".join(commit_line) + f"\n\t{commit_comment}"
     os.system(f'git commit -m "{commit_message}"')
