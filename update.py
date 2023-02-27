9c5720ffa861c59461f33266bd5d6622da7f8565
Maxime <maxime.le-besnerais@epitech.eu>
2023-02-27 14:04:09 +0100

    [FEAT] Better update & version command now available 	update.py: This file has been modified 	giti: File modified 	Committed without a comment

--- update.py
+++ update.py
@@ -1,18 +1,29 @@
 import os
 import json
+import datetime
 
-# raw changelog file address:
 # https://raw.githubusercontent.com/MaximeLeBesnerais/giti_super/main/versions_changelog.json
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 
 
+def get_today():
+    return datetime.datetime.now().strftime("%d/%m/%Y")
+
+
+def time_diff(date1, date2):
+    date1 = datetime.datetime.strptime(date1, "%d/%m/%Y")
+    date2 = datetime.datetime.strptime(date2, "%d/%m/%Y")
+    return (date2 - date1).days
+
+
 class GitiVersion:
-    def __init__(self, version, changelog):
+    def __init__(self, version, changelog, date):
         self.major = version.split(".")[0]
         self.minor = version.split(".")[1]
         self.patch = version.split(".")[2]
         self.changelog = changelog
+        self.date = date
 
     def equals(self, other):
         return self.major == other.major and self.minor == other.minor and self.patch == other.patch
@@ -38,13 +49,11 @@ class GitiVersion:
         return f"{self.major}.{self.minor}.{self.patch}"
 
 
-def giti_get_version():
-    json_file = os.popen("~/.giti/versions_changelog.json").read()
-    json_file = json.loads(json_file)
-    return GitiVersion(json_file["version"], json_file["changelog"])
+def giti_get_version(json_file):
+    return GitiVersion(json_file["version"], json_file["changelog"], json_file["date"])
 
 
-def proceed(none):
+def proceed():
     if os.path.exists(f"{script_path}/update.sh"):
         os.system(f"{script_path}/update.sh")
         exit()
@@ -54,14 +63,13 @@ def proceed(none):
         exit()
 
 
-def giti_update():
-    current_version = giti_get_version()
+def giti_update(silent=False):
+    current_version = giti_get_version(json.loads(open(f"{script_path}/versions_changelog.json").read()))
     print(f"Current version: {current_version.version()}")
-    # get the latest version of giti from the GitHub repository
     json_file = os.popen(
-        'curl https://raw.githubusercontent.com/MaximeLeBesnerais/giti_super/main/versions_changelog.json').read()
+        'curl -s https://raw.githubusercontent.com/MaximeLeBesnerais/giti_super/main/versions_changelog.json').read()
     json_file = json.loads(json_file)
-    latest_version = GitiVersion(json_file["version"], json_file["changelog"])
+    latest_version = GitiVersion(json_file["version"], json_file["changelog"], json_file["date"])
     if current_version.is_newer(latest_version):
         print("You are using a newer version of GITI than the latest one")
         exit()
@@ -70,4 +78,11 @@ def giti_update():
         exit()
     print(f"Latest version: {latest_version.version()}")
     print("The old version of GITI will be deleted and the new one will be downloaded")
-    proceed(None)
+    proceed()
+
+
+def giti_version():
+    current_version = giti_get_version(json.loads(open(f"{script_path}/versions_changelog.json").read()))
+    print(f"Current version: {current_version.version()}")
+    print(f"Last updated {time_diff(current_version.date, get_today())} days ago ({current_version.date})")
+    print(f"Changelog: {current_version.changelog}")
