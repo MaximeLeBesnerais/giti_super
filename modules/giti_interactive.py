a4480387bf52338bcac18715f8e55057984b1684
Maxime <maxime.le-besnerais@epitech.eu>
2023-03-13 12:10:57 +0100

    [REFACTO] Moved functional files in module folder 	modules/: This file has been modified 	Committed without a comment

---
+++ modules/giti_interactive.py
@@ -0,0 +1,85 @@
+import os
+from modules.powered_g import status_letters
+
+
+def parse_selection(selection: str, upper_limit) -> [int]:
+    if selection == "":
+        return []
+    selection = selection.split()
+    try:
+        selection = [int(s) for s in selection]
+    except ValueError:
+        print("Selection must be a number")
+        return []
+    if max(selection) > upper_limit:
+        print("Selection out of range")
+        exit()
+    return selection
+
+
+def get_files_selection(file_list) -> [str]:
+    selection: [str] = []
+    adding = True
+    while adding:
+        print("Select a file to commit")
+        for i, file in enumerate(file_list):
+            print(f"\t{i + 1}. {file}")
+        print("\t0. Done (or press enter)")
+        try:
+            selection_input = input("Selection: ")
+        except EOFError:
+            print("\nInterrupted")
+            return []
+        selection_input = parse_selection(selection_input, len(file_list))
+        if len(selection_input) == 0:
+            adding = False
+        else:
+            for i in selection_input:
+                selection.append(file_list[i - 1])
+            for i in selection_input:
+                file_list.pop(i - 1 - i)
+            if len(file_list) == 0:
+                adding = False
+                print("All files have been selected")
+            if len(file_list) == 1:
+                print("There is only one file left to select")
+                print(f"Do you want to select {file_list[0]}? (y/n)")
+                selection_input = input("Selection: ")
+                if selection_input.lower() == "y":
+                    selection.append(file_list[0])
+                adding = False
+    return selection
+
+
+def giti_interactive():
+    git_log = os.popen('git status -s').readlines()
+    git_log = [line[:-1] for line in git_log]
+    commit_line = []
+    if len(git_log) == 0:
+        print("There are no changes to commit")
+        exit()
+    if len(git_log) > 30:
+        print("There are too many changes to commit with interactive mode")
+        print("Please use the all mode instead, or individually commit files")
+        exit()
+    file_list = [line[3:] for line in git_log]
+    selection = get_files_selection(file_list)
+    if len(selection) == 0:
+        print("There are no files to commit")
+        exit()
+    for i in selection:
+        os.system(f'git add {i}')
+        commit_line.append(f"\t{i}")
+    type_commit = input("What type of commit is this?   ")
+    if type_commit == "":
+        type_commit = "OTHER"
+    title = input("Title: ")
+    if title == "":
+        title = f"[{type_commit.upper()}] For {len(selection)} {'file' if len(selection) == 1 else 'files'}"
+    else:
+        title = f"[{type_commit.upper()}] {title}"
+    comment = input("Comment: ")
+    if comment == "":
+        comment = f"Committed without comment"
+    commit_message = f"{title}\n" + "\n".join(commit_line) + f"\n\t\t{comment}"
+    os.system(f'git commit -m "{commit_message}"')
---
+++ modules/powered_g.py
@@ -0,0 +1,174 @@
+import os
+
+status_letters = {
+    'M': 'This file has been modified',
+    'A': 'This is a new file',
+    'D': 'This is a file suppression',
+    'R': 'This file has been renamed or moved',
+    'C': 'This file has been copied',
+    'U': 'Due to a merge conflict, this file is unmerged',
+    '??': 'As far as git is concerned, this file is untracked'
+}
+
+command_types = {
+    "fix": "[BUG FIX]",
+    "bug": "[BUG FIX]",
+    "feat": "[FEAT]",
+    "docs": "[DOCUMENTATION]",
+    "wip": "[WIP]",
+    "header": "[HEADER]",
+    "assets": "[ASSETS]",
+    "make": "[MAKEFILE]"
+}
+
+
+def giti_all(potential_title):
+    os.system('git add .')
+    git_status = os.popen('git status -s').readlines()
+    git_status = [line[:-1] for line in git_status]
+    commit_line = []
+    if len(git_status) == 0:
+        print("There are no changes to commit")
+        exit()
+    for line in git_status:
+        status_letter = line[:2].strip()
+        status_comment = status_letters[status_letter]
+        commit_line.append(f"\t{line[3:]}: {status_comment}")
+    if potential_title == "":
+        commit_message = f"[ALL] {len(git_status)} {'file' if len(git_status) <= 1 else 'files'} changed\n" + \
+                         "\n".join(commit_line)
+    else:
+        commit_message = f"[ALL] {potential_title}\n" + "\n".join(commit_line)
+    os.system(f'git commit -m "{commit_message}"')
+
+
+def giti_makefile(potential_title):
+    git_log = os.popen('git status -s').readlines()
+    git_log = [line[:-1] for line in git_log]
+    commit_line = []
+    if len(git_log) == 0:
+        print("There are no changes to commit")
+        exit()
+    for line in git_log:
+        if "Makefile" in line:
+            os.system(f'git add {line[3:]}')
+            letter = line[:2].strip()
+            commit_line.append(f"\t{line[3:]}: {status_letters[letter]}")
+    if len(commit_line) == 0:
+        print("There are no Makefile to commit")
+        exit()
+    if potential_title == "":
+        commit_message = f"[MAKEFILE] {len(commit_line)} {'file' if len(commit_line) <= 1 else 'files'} changed\n" + \
+                         "\n".join(commit_line)
+    else:
+        commit_message = f"[MAKEFILE] {potential_title}\n" + "\n".join(commit_line)
+    os.system(f'git commit -m "{commit_message}"')
+
+
+def giti_header(potential_title):
+    git_log = os.popen('git status -s').readlines()
+    git_log = [line[:-1] for line in git_log]
+    header_extensions = [".h", ".hpp", ".hxx", ".hh"]
+    commit_line = []
+    if len(git_log) == 0:
+        print("There are no changes to commit")
+        exit()
+    for line in git_log:
+        if line[3:].endswith(tuple(header_extensions)):
+            os.system(f'git add {line[3:]}')
+            letter = line[:2].strip()
+            commit_line.append(f"\t{line[3:]}: {status_letters[letter]}")
+            continue
+    if len(commit_line) == 0:
+        print("There are no header files to commit")
+        exit()
+    if potential_title == "":
+        commit_message = f"[HEADER] {len(commit_line)} {'file' if len(commit_line) <= 1 else 'files'} changed\n" + \
+                         "\n".join(commit_line)
+    else:
+        commit_message = f"[HEADER] {potential_title}\n" + "\n".join(commit_line)
+    os.system(f'git commit -m "{commit_message}"')
+
+
+def giti_coding_style(files):
+    git_log = os.popen('git status -s').readlines()
+    git_log = [line[:-1] for line in git_log]
+    commit_line = []
+    if len(git_log) == 0:
+        print("There are no changes to commit")
+        exit()
+    for line in git_log:
+        if line[3:] in files:
+            os.system(f'git add {line[3:]}')
+            letter = line[:2].strip()
+            commit_line.append(f"\t{line[3:]}: {status_letters[letter]}")
+            continue
+    if len(commit_line) == 0:
+        print("There are no files to commit")
+        exit()
+    commit_message = f"[CODING STYLE] {len(commit_line)} {'file' if len(commit_line) <= 1 else 'files'} changed\n" + \
+                     "\n".join(commit_line)
+    os.system(f'git commit -m "{commit_message}"')
+
+
+def giti_ignore(potential_title):
+    if not os.path.exists('.gitignore'):
+        print("There is no .gitignore file to commit")
+        exit()
+    git_log = os.popen('git status -s').readlines()
+    commit_line = []
+    for line in git_log:
+        if ".gitignore" in line:
+            os.system(f'git add {line[3:]}')
+            letter = line[:2].strip()
+            commit_line.append(f"\t{line[3:]}: {status_letters[letter]}")
+    if potential_title == "":
+        commit_message = f"[IGNORE] Modified .gitignore"
+    else:
+        commit_message = f"[IGNORE] {potential_title}"
+    os.system(f'git commit -m "{commit_message}"')
+
+
+def giti_del(potential_title):
+    git_log = os.popen('git ls-files --deleted').readlines()
+    git_log = [line[:-1] for line in git_log]
+    if len(git_log) == 0:
+        print("There are no deleted files to commit")
+        exit()
+    commit_line = []
+    for line in git_log:
+        os.system(f'git add {line}')
+        commit_line.append(f"\t{line}: Deleted file")
+    if potential_title == "":
+        commit_message = f"[DELETION] {len(commit_line)} {'file' if len(commit_line) <= 1 else 'files'} removed\n" + \
+                         "\n".join(commit_line)
+    else:
+        commit_message = f"[DELETION] {potential_title}\n" + "\n".join(commit_line)
+    os.system(f'git commit -m "{commit_message}"')
+
+
+def generic_giti(tag_key, title, comment, files: list):
+    if files == "":
+        print("No files to commit")
+        exit()
+    commit_line = []
+    git_log = os.popen('git status -s').readlines()
+    git_log = [line[:-1] for line in git_log]
+    if len(git_log) == 0:
+        print("There are no changes to commit")
+        exit()
+    tag = command_types[tag_key] if tag_key in command_types else f"[{tag_key.upper()}]"
+    for file in files:
+        os.system(f'git add {file}')
+        letter = git_log[0][:2].strip()
+        commit_line.append(f"\t{file}: {status_letters[letter]}")
+    if title == "":
+        potential_title = f"{tag} {len(files)} {'file' if len(files) <= 1 else 'files'} changed\n"
+    else:
+        potential_title = f"{tag} {title}\n"
+    if comment != "":
+        commit_line.append(f"\t\t{comment}")
+    else:
+        commit_line.append(f"\tCommitted without a comment")
+    commit_message = potential_title + "\n".join(commit_line)
+    os.system(f'git commit -m "{commit_message}"')
