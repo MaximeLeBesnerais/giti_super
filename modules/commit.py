fe52cfe2eac303cc66551f1465a63986972c0ac0
Maxime <maxime.le-besnerais@epitech.eu>
2023-03-15 18:52:27 +0100

    [FIX] [FIX] 1 file changed 	Files: 		modules/commit.py: Modified
    	Comment:
    		Commit made without comment

--- modules/commit.py
+++ modules/commit.py
@@ -6,7 +6,7 @@ git_dict = {"A": "Added", "M": "Modified", "D": "Deleted", "R": "Renamed", "C": 
 
 def associate(letters):
     text = ""
-    if letters[0] == letters[1]:
+    if len(letters) > 1 and letters[0] == letters[1]:
         if letters[0] in letters:
             return git_dict[letters[0]]
         return "Unknown"
@@ -23,6 +23,12 @@ def file_line(log_line):
     tab = log_line.split(" ")
     return f"{tab[1]}: {associate(tab[0])}"
 
+def array_accept(array, values):
+    new_array = []
+    for i in range(len(array)):
+        if any (value in array[i] for value in values):
+            new_array.append(array[i])
+    return new_array
 
 class Commit:
     def __init__(self, tag, title, comment, files,
@@ -34,39 +40,36 @@ class Commit:
         self.valid : bool = False
         self.status = subprocess.run(status.split(), stdout=subprocess.PIPE).stdout.decode("utf-8")
     
-    def check_files(self):
-        for file in self.files:
-            if file not in self.status:
-                self.valid = False
-        self.valid = True
-    
     def __str__(self):
         tag = f"[{self.tag.upper()}]"
         if self.title != "":
             title = f"{self.title}"
         else:
             title = f"{tag} {len(self.files)} {'file' if len(self.files) <= 1 else 'files'} changed"
-        files = [file_line(file) for file in self.status.split() if file != ""]
-        files_msg = f"Files:" + "\n\t\t".join(files)
+        array = self.status.splitlines()
+        array = array_accept(array, self.files)
+        files = [file_line(line.strip()) for line in array]
+        files_msg = f"\tFiles:\n\t\t" + "\n\t\t".join(files)
         if self.comment != "":
-            comment = f"{self.comment}"
+            comment = f"\tComment:\n\t\t{self.comment}"
         else:
-            comment = "Commit made without comment"
+            comment = "\tComment:\n\t\tCommit made without comment"
         commit_msg = f"{tag} {title}\n{files_msg}\n\n{comment}"
         return commit_msg
     
     def commit(self):
         if self.valid:
-            subprocess.run(["git", "add", self.files])
-            subprocess.run(["git", "commit", "-m", self.__str__()])
+            array = ["git", "add"]
+            array.extend(self.files)
+            subprocess.run(array)
+            subprocess.run(["git", "commit", f"-m {self}"])
             return True
         print(f"Could not proceed with this commit")
         print(f"Reason: A file is not in the git status")
         return False
     
-def make_commit(tag, title, comment, files):
-    commit = Commit(tag, title, comment, files)
-    if commit.check_files():
-        commit.commit()
-        return True
-    return False
+def make_commit(tag, title, comment, files, status = "git status --porcelain"):
+    commit = Commit(tag, title, comment, files, status)
+    commit.valid = True
+    commit.commit()
+    return True
