51ef44133d20f91a1433c10f9954cd232a41ede4
Maxime Le Besnerais <maxime.le-besnerais@epitech.eu>
2025-02-21 03:51:39 +0800

    [WIP] Add Tool action resolver (first steps)

---
+++ include/ToolActionResolver.hpp
@@ -0,0 +1,12 @@
+/*
+Name: ToolActionResolver.hpp               
+by: Maxime                                 
+mail: maxime.le-besnerais@epitech.eu       
+description:                               
+created: 21/02/2025 00:14:27               
+*/
+
+#ifndef TOOLACTIONRESOLVER_HPP
+    #define TOOLACTIONRESOLVER_HPP
+    
+#endif // TOOLACTIONRESOLVER_HPP
---
+++ src/ToolActionResolver.cpp
@@ -0,0 +1,2 @@
+#include <ToolActionResolver.hpp>
+
