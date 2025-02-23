6e8faa681d146e8e9bb158eba35a17f64995438d
Maxime <maxoulebesnerais@epitech.eu>
2025-02-23 21:42:52 +0800

    [FEAT] Add utility and tool parser structures to giti_parser.hpp

--- include/giti_parser.hpp
+++ include/giti_parser.hpp
@@ -12,4 +12,18 @@ created: 20/02/2025 03:18:42
     cxxopts::Options createUtilityParser(void);
     cxxopts::Options createToolParser(void);
     cxxopts::Options createCommandLineParser(void);
+    typedef struct {
+        bool _all;
+        bool _build;
+        bool _ignore;
+        bool _del;
+        bool _other;
+    } _utilityParser;
+    
+    typedef struct {
+        bool _version;
+        bool _update;
+        bool _force;
+        bool _remove;
+    } _toolParser;
 #endif /* !GITI_PARSER_H_ */
